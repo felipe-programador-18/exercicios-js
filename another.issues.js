a069e4ce706eb16c9e8bc1f0e4d371bdf1c87618
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2021-11-04 12:07:05 -0300

    c1-adding  past issues

---
+++ another.issues.js
@@ -0,0 +1,7 @@
+const enough = [1,2,3,1,2,1,3,43,3434,23]
+
+const number = (function(item){
+  return item <1000
+})
+
+console.log(enough.filter(number))
---
+++ notas.js
@@ -0,0 +1,5 @@
+//notas alunos!!!
+const notas = [7,6]
+const media = (one, two,) => (one + two) /2
+
+console.log(notas.reduce(media))
