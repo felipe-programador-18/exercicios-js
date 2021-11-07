ee48159e982ebeccfd2d66d14ce66ae457e1c691
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2021-11-07 10:01:49 -0300

    c3-adding issues about foreac

---
+++ foreac.js
@@ -0,0 +1,8 @@
+const right = ["more", 'sure', "reporting",'troubleshooting']
+
+right.forEach(Element => console.log(Element + " one " + " two"))
+
+const Number = [213,123,1,2,34,54]
+
+Number.forEach(Element => console.log(Element **2))
+
