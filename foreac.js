a90e62b5538db62e485dfc8374af2f94a0e0c55a
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2021-11-08 06:13:09 -0300

    c5-adding more issues

--- foreac.js
+++ foreac.js
@@ -6,3 +6,9 @@ const Number = [213,123,1,2,34,54]
 
 Number.forEach(Element => console.log(Element **2))
 
+
+
+const number = [10,20,30,40,11,43]
+
+number.forEach(Element=> console.log(Element**3))
+
