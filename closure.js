c122f677498439cb9f6355f8633cefa174d9fb38
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2021-11-10 06:17:36 -0300

    c7-adding solve about closure

---
+++ closure.js
@@ -0,0 +1,13 @@
+function kinda (more) {
+
+    let count = more ;
+    return function () {
+        count++
+        return count
+    }
+}
+
+//this closure i can adding another value and adding another thing how numbers!!!
+const caught = kinda(10*5)
+const insight  = kinda(2)
+console.log(caught(), caught(), insight())
