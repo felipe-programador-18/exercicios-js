714eeea34ec9c8bd3d94c3efe664283eaada4783
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2021-11-13 16:47:11 -0300

    c9-modify interpolacao de strings

---
+++ interpolacao.js
@@ -0,0 +1,11 @@
+const objeto = {
+    name: 'Felipe',
+    sobrename: 'martins',
+    age: 26,
+    city:'Porto alegre',
+    stage: 'Rio Grande Do Sul'
+}
+
+
+console.log(`My name is  ${objeto.name} and i have ${objeto.age} years, currently I living
+in the city ${objeto.city} that stay in ${objeto.stage} and avoid in that ${objeto.sobrename}`)
