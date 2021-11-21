f7e413fcb00457efe0c7f107ce3df798f37fc862
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2021-11-21 11:55:34 -0300

    c19-adding foreach

--- foreac.js
+++ foreac.js
@@ -12,3 +12,8 @@ const number = [10,20,30,40,11,43]
 
 number.forEach(Element=> console.log(Element**3))
 
+
+
+const Kinda = [32,123,3213,12,21,1,23]
+
+Kinda.forEach(Element=> console.log(Element *50))
