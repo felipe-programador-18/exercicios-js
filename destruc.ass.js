931041776f9ad49c36ba05a9a3076a2abdc0bf34
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2021-11-29 18:38:14 -0300

    c22-adding new issues for me

---
+++ destruc.ass.js
@@ -0,0 +1,10 @@
+const obj = {
+    name:'martins',
+    sobrename: 'felipe',
+    city:'porto alegre'
+}
+
+
+// traning about destructing assisment!!
+const {name, sobrename} = obj
+console.log(obj.name, sobrename)
---
+++ localstora.js
@@ -0,0 +1,5 @@
+
+const pega = localStorage.setItem('gat')
+const gat = localStorage.getItem('gat')
+
+console.log(gat)
