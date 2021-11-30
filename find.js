501d5d04002529dd237199890a88762bb09ae7f9
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2021-11-30 06:49:42 -0300

    c23- adding issues about find

---
+++ find.js
@@ -0,0 +1,16 @@
+//the command find he return one the first element before this rules!!!
+
+const vetor = [5,6,7,20,43]
+const found = vetor.find(Element => Element >10)
+console.log(found)
+
+
+
+const more = function(item){
+    return item <1000
+}
+
+
+const fila = [323,232,4335,5654,7777].filter(more)
+console.log(fila)
+
