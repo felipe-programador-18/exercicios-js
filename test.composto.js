00423f2645914f55ac9bb53ecdf9022d61cf824a
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2021-12-04 19:03:44 -0300

    c27- thought in resolve issues

--- test.composto.js
+++ test.composto.js
@@ -1,15 +1,25 @@
 const CalJuros = (c,i,T) =>{
-    c= 1000
-    i=0.50
-    T=24 
-    Montante = c*(1+i)*T
-    console.log(Montante)
-    Juros = Montante - c
-    console.log(Juros)
+     Montante = c*(1+i)*T
+     Juros = Montante-c
+     console.log(Juros) 
 }
+
+
+
+CalJuros()
+const Soma = CalJuros(100,5.43,24,(Montante-100))
+console.log(Soma)
+
 // i taxa
 // T tempo mes ou ano
 // C capital
 // J juros
 
+//console.log(Montante)
+//Juros = Montante - c
+//console.log(Juros)
+
+//c= 1000
+//i=0.50
+//T=24 
 CalJuros()
