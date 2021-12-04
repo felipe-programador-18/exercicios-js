269be4f8fba01949ac749da4e4d480f24596b402
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2021-12-04 17:39:29 -0300

    c26 - resolve about invoice compost

--- test.composto.js
+++ test.composto.js
@@ -1,10 +1,15 @@
 const CalJuros = (c,i,T) =>{
-    c= 100
+    c= 1000
     i=0.50
     T=24 
     Montante = c*(1+i)*T
     console.log(Montante)
+    Juros = Montante - c
+    console.log(Juros)
 }
-
+// i taxa
+// T tempo mes ou ano
+// C capital
+// J juros
 
 CalJuros()
