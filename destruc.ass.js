0684d604887ae86b470974deb3aa98c2e19db915
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2021-12-04 17:16:39 -0300

    c25-try about destrucment assyment

--- destruc.ass.js
+++ destruc.ass.js
@@ -8,3 +8,9 @@ const obj = {
 // traning about destructing assisment!!
 const {name, sobrename} = obj
 console.log(obj.name, sobrename)
+
+
+
+const vetor = ['martins', 'without','avoid', 'eagers']
+const [subvetor] = vetor
+console.log(vetor.subvetor)
