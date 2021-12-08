623a281b644231d6e8a73aac8d05763a384385c0
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2021-12-08 19:12:45 -0300

    c30-adding new modify

---
+++ pin.js
@@ -0,0 +1,12 @@
+//ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything 
+//but exactly 4 digits or exactly 6 digits.
+//If the function is passed a valid PIN string, return true, else return false.
+
+function validatePIN (pin) {
+    return  pin == 4 || 6
+
+  }
+  const Vetor = [1,2,3].filter(validatePIN)
+
+  console.log(Vetor)
+   
