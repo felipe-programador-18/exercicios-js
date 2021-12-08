4ed8cdb8b794a9b53c22374f6d3e14310cedaae1
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2021-12-08 19:00:55 -0300

    c29-solve issues for codewars

---
+++ code.js
@@ -0,0 +1,15 @@
+//Complete the square sum function so that it squares each number passed into it and then sums the results together.
+//For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+function squareSum(){
+    let Sun =[1,2,2,3]
+    const Caught = (vetorone, vetortwo) => vetorone + vetortwo **2
+    return console.log(Sun.reduce(Caught))
+}
+
+squareSum()
+
+
+//const squereSun = [0,3,4,5]
+//const Caught = (vetorone, vetortwo) => vetorone + vetortwo **2
+//console.log(squareSun.reduce(Caught))
