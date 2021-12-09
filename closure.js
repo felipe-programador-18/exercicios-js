126dd558ad2d781eff8c2cc2e8b6704ad732ba28
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2021-12-09 19:13:23 -0300

    c32 -adding new somethigs

--- closure.js
+++ closure.js
@@ -10,4 +10,4 @@ function kinda (more) {
 //this closure i can adding another value and adding another thing how numbers!!!
 const caught = kinda(10*5)
 const insight  = kinda(2)
-console.log(caught(), caught(), insight())
+console.log(caught(), caught(), insight(), insight())
