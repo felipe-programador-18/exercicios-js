8df539e5669dfdd0fae52b8cdcf8decfc472a19e
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2021-12-12 17:33:17 -0300

    c35- traning more and more about map

--- filter.js
+++ filter.js
@@ -5,3 +5,14 @@ const More = function(item){
 const vetor = [321,23,1,323,12,32,456,65,10,5,6].filter(More)
 
 console.log(vetor)
+
+
+const troubleshooting = [12,123,31,232,5435,6756,123,534]
+
+
+//use MAP !!!
+const array = [1,2,3,4,5,6]
+const Double = array.map(function (item){
+    return  {original:item , triplicado :item *3 }
+})
+console.log(array, Double)
