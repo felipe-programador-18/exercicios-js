2285ef68ee735862c66b8f1a97c52e7ad10f81c2
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2021-12-12 19:43:37 -0300

    c39- adding new issues dont resolve yet

---
+++ potencials.js
@@ -0,0 +1,17 @@
+//A Narcissistic Number is a positive number which is the sum of its own digits,
+//each raised to the power of the number of digits in a given base. 
+//In this Kata, we will restrict ourselves to decimal (base 10).
+//For example, take 153 (3 digits), which is narcisstic:
+//1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+
+const Vetor = [1,6,5,2]
+//const thuth = (one , two) => one + two
+
+const allow = Vetor.map(function(item){
+    return {teste: (item**4)}    
+})
+
+const pensa = Vetor.reduce(item+1)
+//console.log(Vetor.reduce(thuth))
+console.log(allow)
+console.log(pensa)
