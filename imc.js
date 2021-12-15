19e4e8c10ac965a1142ec27267a275db432fd684
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2021-12-15 19:47:30 -0300

    c44 training more issues for solve

--- imc.js
+++ imc.js
@@ -6,8 +6,16 @@ console.log((total).toFixed(2))
 
 
 
-let thuoght = 20
 
-for(let i = 0; i<100; i++){
-    console.log('test here now' +i)
+const main = (params) => {
+    var Teste = 'abcdefghijklmnopqrstuvxz'
+    var Text = ''
+
+    for(var i = 0; i < 10000; i++){
+        const numero = Math.floor(Math.random()* Teste.length)
+        Text += Teste.substring(numero, numero+1);
+    }
+    return Text
 }
+
+console.log(main())
