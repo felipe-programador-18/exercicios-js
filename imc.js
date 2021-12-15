9581265c6e3a882c627944634d6cdfd3d6257240
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2021-12-15 18:31:23 -0300

    c43- training about for

--- imc.js
+++ imc.js
@@ -3,3 +3,11 @@ let altura = 1.70
 let total = peso/ (altura*altura)
 
 console.log((total).toFixed(2))
+
+
+
+let thuoght = 20
+
+for(let i = 0; i<100; i++){
+    console.log('test here now' +i)
+}
