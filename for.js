a99b40951adec1205d4df6d0efbb90d2451faed2
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2021-12-15 20:08:25 -0300

    c47-training more and more always

--- for.js
+++ for.js
@@ -12,3 +12,20 @@ const Four = () =>{
 }
 
 console.log(Four())
+
+
+
+const Practice = () =>{
+
+    let Morein = 10
+    let Least = 0
+
+    for(var i = 0 ; i<10000; i++){
+     const Caught = Math.floor(Math.random()* Morein.lenght)
+     Least += Morein.toFixed(Caught, Caught+1)
+
+     return Least
+    }
+}
+
+console.log(Practice())
