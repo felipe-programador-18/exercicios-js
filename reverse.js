d3a108cd8171e6025018fe222b6706d6e3480815
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2021-12-19 20:04:54 -0300

    c57 descover reverse is very interative

---
+++ reverse.js
@@ -0,0 +1,5 @@
+const Value = [1,2,3,4,5,6,7,8]
+console.log('Value is: ' + Value)
+
+const Makedecision = Value.reverse()
+console.log('This value of vector is change order of back to front',Makedecision)
