75df28be2e3ddf482c3cc064f2f9f382ddb5e84e
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2021-12-19 19:49:53 -0300

    c55 traning splint and about join

---
+++ splintjoin.js
@@ -0,0 +1,6 @@
+const name = 'martins'
+// the command splint he breaking the words in various piece!!
+console.log(name.split(' '))
+
+const Sobrename = 'Felipe Programer'
+console.log(Sobrename.split(' ').join(' <==> '))
