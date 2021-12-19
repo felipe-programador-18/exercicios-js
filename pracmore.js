909789d1fe33c5e0b5db6f53843c1dd1858230fa
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2021-12-19 11:25:49 -0300

    c52- practice about promise and async await

--- pracmore.js
+++ pracmore.js
@@ -13,3 +13,20 @@ const Caught = (value) => {
    console.log('teste here now', value)
 }
 cost.forEach(Caught)
+
+
+
+
+
+const Wakeup = pense => new Promise(resolve =>{
+    setTimeout(resolve, pense)
+})
+
+const realiza = async() => {
+    console.log('lets right jump');
+    await Wakeup (2000)
+    console.log('hi there, maybe you can seeing for me')
+    await Wakeup(4000)
+    console.log('least more sure')
+}
+realiza()
