680b1cd307a5fd5e485ef621842a905db1f9b5ea
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2021-12-19 20:47:23 -0300

    c58- try againg about flotmap

---
+++ flap.map.js
@@ -0,0 +1,14 @@
+let arr1 = ["it's Sunny in", "", "California"];
+console.log(arr1)
+arr1.map(x => x.split(" "));
+// [["it's","Sunny","in"],[""],["California"]]
+
+arr1.flatMap(x => x.split(" "));
+// ["it's","Sunny","in", "", "California"]
+console.log(arr1)
+
+
+
+//training tomorrow again learning about this!!!
+const Greetw = ['hellow', 'is beatiful', 'attitude'].flatMap('')
+console.log(Greetw)
