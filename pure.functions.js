dd953c7464a351488b64751a51171c955cdb3115
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-07 16:22:32 -0300

    c68-training more and more javascript

---
+++ pure.functions.js
@@ -0,0 +1,10 @@
+//practice about pure functions!!!!
+const caught = (num1, num2) => num1 + num2
+const together = (caught(5000,323123))
+
+console.log(together)
+
+const Moretime = (one, two) => one * two
+const receive = (Moretime(200,42343))
+
+console.log(receive)
