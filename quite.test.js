2087d24db8e72a486eeda517d37d57b05a803965
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-12 16:27:04 -0300

    c74-adding new something about test code and export module node.js

--- quite.test.js
+++ quite.test.js
@@ -5,3 +5,7 @@ const receive = require('./quite.more')
 test('test more an once', () => {
     expect(receive.multiple(100,332)).toBe(33200)
 })
+
+test('seeing this result of more three and more four', () =>{
+    expect(receive.More(500,40)).toBe(540)
+})
