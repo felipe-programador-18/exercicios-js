3b68c21c8350e6ffab2244098c12d7dc50d57355
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-22 20:59:30 -0300

    c77- interpolation of string and destructment assyment

--- desctrassiment.js
+++ desctrassiment.js
@@ -19,3 +19,20 @@ const More = {
 
 const {brandone , brandthree } = More
 console.log(More.brandone, brandthree)
+
+
+
+
+const laught ={
+    trademarks:'Facebook',
+    trademarksone: 'twitter'
+}
+
+const Phase = `is bad ${laught.trademarks} net social, always prefer net ${laught.trademarksone}
+is much better always`
+console.log(Phase)
+
+const {seeing, seeingset} = laught
+console.log(laught.trademarks, laught.trademarksone)
+
+
