87e360907b61790c62020d4cd6d95c1a3860f833
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-22 20:47:26 -0300

    c75 - create and practice about filter and redeucer

--- splintjoin.js
+++ splintjoin.js
@@ -8,3 +8,17 @@ console.log(Sobrename.split(' ').join(' <==> '))
 
 const tenure = ['Programing', 'thouhgt']
 console.log(tenure.join(` ${Sobrename } `))
+
+
+const More =(item) => {
+    return item < 200
+}
+
+const String = '+'
+
+const together = [123,123,3123,123,1231,231].filter(More)
+console.log(together.join(` ${String} `))
+
+const Union = (one, two) => one +two
+const CaughtMe = together.reduce(Union)
+console.log(CaughtMe)
