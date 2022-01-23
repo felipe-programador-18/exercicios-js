b60e5a0bbee18c09d5945d74f2ce32293dded770
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-22 21:12:43 -0300

    c80 - practice about reverse

--- reverse.js
+++ reverse.js
@@ -3,3 +3,9 @@ console.log('Value is: ' + Value)
 
 const Makedecision = Value.reverse()
 console.log('This value of vector is change order of back to front',Makedecision)
+
+
+
+
+const Vetor = [11,12,13,14,15,16,17]
+console.log('eu quero contagem regressiva ' + Vetor.reverse())
