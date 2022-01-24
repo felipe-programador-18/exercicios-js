df3ac8bfba4b475a8752462520b8f26c0d00f5cc
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-24 13:48:41 -0300

    c90- practice little about array

---
+++ arr.js
@@ -0,0 +1,15 @@
+const array = [1,2,3,4,5,6,7,8]
+
+const caught = (index, array, value) =>{
+ console.log(index, array,value)
+}
+
+array.forEach(caught)
+
+
+const Kinda = [32,123,3213,12,21,1,23]
+Kinda.forEach(Element=> console.log(Element *50))
+
+
+const Family = [1,2,4,56,10,7,8]
+Family.forEach(Element => console.log(Element**2))
