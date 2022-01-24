c0e43f2a6f854542c9404640783196b6fd10f187
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-24 09:50:53 -0300

    c87- adding spring operation

---
+++ spring.operation.js
@@ -0,0 +1,29 @@
+const pesssoa = {
+    name: 'Martins',
+    Sobrename: 'Progamer18',
+    idade: 18
+}
+
+const caught = {...pesssoa}
+caught.idade = 50
+console.log(pesssoa)
+console.log(caught)
+
+
+const test = {
+    aluno: 'Beatriz',
+    matricula: 32133,
+    age: 20
+}
+const together = {...test}
+together.age = 100
+
+console.log(test)
+console.log(together)
+
+const morestest = {
+    studies:' Bianca',
+    matricula: 3423423,
+    age = 50
+}
+
