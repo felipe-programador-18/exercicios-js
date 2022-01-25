b267eeee247278c2c427c58d8c1e76da49031187
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-25 11:18:37 -0300

    c101- practice more little about spring operator

--- moreaboutspring.js
+++ moreaboutspring.js
@@ -1,2 +1,12 @@
 // practice about spring operator!!
 
+const human = {
+    name: 'Programmer',
+    sobrename: 'martins',
+    age: 26
+}
+
+const Together = {...human, email:'machadofelipe2016@outlook.com', ...human}
+Together.celular = '9-89530193'
+
+console.log(Together)
