37ff2d07e0baaa03447856f18bfae209ad229252
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-25 11:08:18 -0300

    c99- remmerber and practice about for

--- practicefor.js
+++ practicefor.js
@@ -4,7 +4,20 @@ for(let Number =10; Number<20; Number++){
     console.log(Number)
 }
 
-
 for(var i =0 ; i<100; i++){
     console.log('count here now', i)
+}
+
+
+//testando os arrays e for junto para laços de repetição//
+var alunos = ["felipe", "joao", "patrick", "felisberto", "maria rita",
+ "betina" ,"felipe martins", "allan","bernardo"];
+
+for(var i of alunos){
+    console.log(i);
+}
+
+var Objeto = ['marints', 'betina', 'tina']
+for(var i of Objeto){
+    console.log(i)
 }
