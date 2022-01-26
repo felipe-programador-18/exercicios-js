2e0aa030425c9f0dfd8c26cf6c7364f587dfdab2
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-26 13:11:59 -0300

    c105- practice and remmeber about map

---
+++ moremap.js
@@ -0,0 +1,13 @@
+
+const together = [12,32,31,3123,123]
+const Aloof = together.map(function(item){
+    return item * 2
+})
+
+console.log(Aloof)
+
+const Pickle = ['more', 'about', 'thought', 'struggling']
+const Phase = Pickle.map(function(item){
+    return item + ` Together more strong`
+})
+console.log(Phase)
