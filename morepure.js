252601c550c0bf5f237804baa015cf316e378922
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-26 13:02:30 -0300

    c104- adding practice more about pure function and interpolation of string

---
+++ morepure.js
@@ -0,0 +1,14 @@
+const Aluno = (studiesone , studiestwo) => ( studiesone + studiestwo)
+console.log(Aluno('martins struggling' , ' beatriz'))
+
+const shoot = (one, two) => (one / two)
+console.log(shoot(122,6))
+
+const Caught = (four, five) => (four ** five)
+const Giveup = Caught(10,5)
+console.log(Giveup)
+
+
+const phase = ` hi there i am together another number ${Giveup}
+`
+console.log(phase)
