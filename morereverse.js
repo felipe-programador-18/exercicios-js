799f4079950855b5151cf61daaaf6994a672dc27
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-26 13:21:03 -0300

    c107- now it works with number

--- morereverse.js
+++ morereverse.js
@@ -3,4 +3,5 @@ const save = Picth.reverse()
 // seeing this it works only number
 console.log('test this command reverse seeing that it works now with phase', save)
 
-const 
+const Union = [1,2,3,4,5,6,7,8,9]
+console.log('i want see', Union.reverse())
