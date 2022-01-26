37487e3213f83140419d67c72299218da96442b3
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-26 13:19:43 -0300

    c106- test and practice about reverse with string

---
+++ morereverse.js
@@ -0,0 +1,6 @@
+const Picth =['the mather is people lot sadly']
+const save = Picth.reverse()
+// seeing this it works only number
+console.log('test this command reverse seeing that it works now with phase', save)
+
+const 
