2185dc186cef29f071afafa7690e3b3865fe835f
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-28 16:45:23 -0300

    c114- adding filter to caught date of born of people

--- remap.js
+++ remap.js
@@ -18,3 +18,18 @@ console.log(Equal)
 const Kinda = [32,123,3213,12,21,1,23]
 
 Kinda.forEach(Element=> console.log(Element *2))
+
+const referral = function(item){
+    return item < 50
+}
+
+const createnew = [12,3,123,54,6,5,49,43,32].filter(referral)
+console.log('teste array and filter!!!', createnew)
+
+//make filter to people born below of year 2000
+const Decade = function(born){
+    return born < 2000 && born > 1975
+}
+
+let Vector = [1995,1996,1999,1976, 1920, 1930].filter(Decade)
+console.log(Vector)
