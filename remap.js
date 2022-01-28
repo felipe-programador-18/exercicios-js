4327cb2e68727196102f3067c60b020f0d0dd782
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-28 16:37:01 -0300

    c114- pratice about map and about foreach

---
+++ remap.js
@@ -0,0 +1,20 @@
+
+//remember practice more about map to challange tomorrow
+
+const resolve =[12,33,4324,123,453]
+
+const Together = resolve.map(function(item){
+    return {teste: item* 2, item} 
+})
+
+console.log(resolve, Together)
+
+
+const Equal = [1,2,43,454,66,342]
+Equal.forEach(Element=> console.log(Element *2))
+console.log(Equal)
+
+
+const Kinda = [32,123,3213,12,21,1,23]
+
+Kinda.forEach(Element=> console.log(Element *2))
