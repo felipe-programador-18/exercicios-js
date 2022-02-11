5c210b5691501d4cd6684bac4c266b002e0fc518
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-11 19:31:38 -0300

    c117- remmeber about readdir

---
+++ red.js
@@ -0,0 +1,18 @@
+
+let Phase = 'I have struggling more and more for the future much better to me'
+let name = 'programer martins'
+let togetherUnion = `let me think ${name} is person lot effors in the life and
+too ${Phase}`
+
+console.log(togetherUnion)
+
+console.log(name)
+
+const fs = require('fs')
+fs.readdir('./', (err, list) =>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log(list)
+    }
+})
