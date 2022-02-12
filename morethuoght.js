7d2efd365b1bc7efab0a2dbd4964f40d8881b655
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-12 10:43:31 -0300

    c123- adding new code to practice more and more

---
+++ morethuoght.js
@@ -0,0 +1,20 @@
+const Novo = [1,2,3,4,5,1,3,4,3,6,7, 'martins', 'martins', 'felipe martins']
+
+const Atencao = (agr, value) => {
+    if(!agr [value]){
+        agr[value]= {
+            val: value,
+            ocurr: 0
+        }
+    }
+    agr[value].ocurr = agr [value].ocurr + 1
+    return agr
+}
+const Contagem = Novo.reduce(Atencao, {})
+const Key= Object.keys(Contagem)
+//test and thought about solution
+console.log(Key)
+
+const Unique = Key.filter( key => Contagem[key].ocurr === 1)
+const Volta = Unique.map(value => Contagem[value].val)
+console.log(Volta)
