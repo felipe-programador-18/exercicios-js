cffb9f87fdd576e2e4b2a0103d1920ac1b5fa2c5
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-12 10:44:05 -0300

    c124- adding new to code to remember more

---
+++ didagain.js
@@ -0,0 +1,16 @@
+const Vetor =  [1,2,3,4,5,1,23,7,8, 'Felipe', 'felipe', 'martins', 'martins']
+
+const Create  = (pai, filho) => {
+    if(!pai[filho]){
+      pai[filho]= {
+        fi : filho,
+        occor : 0
+      }
+    }
+     pai[filho].occor = pai[filho].occor +1
+     return pai
+    
+}
+
+const Get = Vetor.reduce(Create, {})
+console.log(Get)
