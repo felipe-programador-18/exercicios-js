0ba3634951ce93fb7bcb286430dd65226a3539f5
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-21 13:07:23 -0300

    c128- create new map

--- map.js
+++ map.js
@@ -6,3 +6,12 @@ const Test = kinda.map(function(item) {
 })
 
 console.log(kinda,Test)
+
+
+const Together = [1,2,3,4,5,6]
+
+const list = Together.map(function(item) {
+   return  {caught:`<li>  ${item*2} </>`} 
+})
+
+console.log(list)
