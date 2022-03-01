e2ac8047e598c7bdb3357658565a28be65329f53
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-03-01 10:44:00 -0300

    c132- try about readfile

--- readir.js
+++ readir.js
@@ -9,3 +9,12 @@ fs.readdir('./', function(err, list){
     }
 
 })
+
+
+fs.readFile('/', function(err,data){
+    if(err){
+        console.log(err)
+    }else{
+        console.log(data)
+    }
+})
