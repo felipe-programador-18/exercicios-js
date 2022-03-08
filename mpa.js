41283058247629a6ae90157fb5fdd383d64c58e9
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-03-08 13:23:20 -0300

    c134- practice about value and refferd

--- mpa.js
+++ mpa.js
@@ -6,3 +6,19 @@ console.log(union)
 module.exports={
     Soma
 }
+
+const fill = [3213,34,123,545,123]
+
+const Seeing= fill.filter(function(item){
+    return item <1000
+})
+
+console.log(Seeing, fill)
+
+
+// traning about value and reffered
+let Name = {nome:'Felipe Programer', age:12}
+let Fother = Name
+Fother.age = 325
+
+console.log(Name)
