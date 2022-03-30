9dbe09b0a0458c4ca8bc6d9a6b3780f23b5db9ba
Felipe <machadofelipe2016@outlook.com>
2022-03-30 14:21:37 -0300

    c140- adding new code about practice async

--- async.js
+++ async.js
@@ -1,5 +1,16 @@
 //practice more bit about async await!!
 
+function createdNewP(ts){
+    return new Promise((resolve, reject)=>{
+     setTimeout(() => resolve(ts), ts)
+    })
+}
+
+async function caughtHere(){
+    const ts = await createdNewP(4000)
+    console.log('is very import async function', ts)
+}
+
 function tempo(ms){
     return new Promise((resolve, reject) => {
         setTimeout(()=> resolve(ms), ms)
@@ -7,8 +18,30 @@ function tempo(ms){
 }
 
 async function Greetw(){
-    const ms = await tempo(2000)
+    const ms = await tempo(3000)
     console.log('finally', ms)
 } 
-
+console.log(caughtHere())
 console.log(Greetw())
+
+
+
+
+
+function buildNew(sr){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=> resolve(sr), sr)
+    })
+}
+
+async function builtData(){
+   const sr = await buildNew(5000)
+   console.log('test here sr',sr)
+ 
+   const Vector = [1,2,3454,65,3234]
+   const reducer = (one,two) => one+two
+   console.log(Vector.map(reducer))
+    
+}
+
+console.log(builtData())
