9a5bcc532fa8568c73d9254b1556368fe2b28a11
Felipe <machadofelipe2016@outlook.com>
2022-04-03 13:35:24 -0300

    c145-adding practice about foreach and map

---
+++ foereach.js
@@ -0,0 +1,10 @@
+
+const Vectoers = [123,123,13,45,123]
+Vectoers.forEach(Element => console.log(Element*3))
+
+const Health = [123,4,23432,123,123123,154312]
+const Pega = Health.map(function(item){
+    return item *2
+})
+
+console.log(Pega)
