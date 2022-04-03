a0c3ebb40599941fbedbafa1ba8a69bae2bdbc97
Felipe <machadofelipe2016@outlook.com>
2022-04-03 17:44:10 -0300

    c146-adding date about destructment assiment

--- destruc.ass.js
+++ destruc.ass.js
@@ -14,3 +14,13 @@ console.log(obj.name, city)
 const vetor = ['martins', 'without','avoid', 'eagers']
 const [subvetor] = vetor
 console.log(vetor.subvetor)
+
+
+const brand = {
+    nome: 'nike',
+    nomeone:'addidas',
+    nomethree:'pumas'
+}
+
+const {nome, nomeone, nomethree} = brand
+console.log(brand.nome)
