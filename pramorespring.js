335a141e5b1f1cf6f33fc56df8872cabe362166b
Felipe <machadofelipe2016@outlook.com>
2022-04-03 13:32:08 -0300

    c144- adding more exercicies about spreing operation

---
+++ pramorespring.js
@@ -0,0 +1,10 @@
+//practice little about spring operator!!!
+const object = {
+    name:"programmer",
+    age:24,
+    adress:'avenu os guidance'
+}
+
+const Union = {...object, email:'machadofelipe1026@outltook.com', employer:'programador felipe18' , ...object}
+console.log(Union)
+
