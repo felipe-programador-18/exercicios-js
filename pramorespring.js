2454444c31e59c1e0e8dc98213e44ef7d0d2b52b
Felipe <machadofelipe2016@outlook.com>
2022-04-13 18:43:16 -0300

    c151- adding and practice more about spring operation

--- pramorespring.js
+++ pramorespring.js
@@ -8,3 +8,9 @@ const object = {
 const Union = {...object, email:'machadofelipe1026@outltook.com', employer:'programador felipe18' , ...object}
 console.log(Union)
 
+
+const SpringOperation = { employer:'Felipe Programmer', nacionality:"Brazilian", age:29}
+
+
+const Toeqaull =  {...SpringOperation, waded:500000, ...SpringOperation, conjugy:'single'}
+console.log(Toeqaull)
