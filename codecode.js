70d08963d746d2aa79645c67f731eef3f496d1ec
Felipe <machadofelipe2016@outlook.com>
2022-04-13 18:33:49 -0300

    c148- adding practice about object key

--- codecode.js
+++ codecode.js
@@ -2,3 +2,7 @@ const count =['aba',12,1,1,1]
 const test = (redone, redtwo) => redone + redtwo
 console.log(count.reduce(test))
 console.log(Object.keys(count))
+
+
+const Vector =[name='felipe', subname='programmer18']
+console.log(Object.keys(Vector))
