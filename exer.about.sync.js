652185d4121d19c9e49155482666c39125cc5ba9
Felipe <machadofelipe2016@outlook.com>
2022-04-27 15:33:46 -0300

    c163-practice about process async of temporalize

---
+++ exer.about.sync.js
@@ -0,0 +1,14 @@
+// Temporizadores
+//test in the temporizadores
+
+console.log(1)
+setTimeout( () => {
+  console.log('ola em 2 segundos...')
+ }, 2)
+console.log(2)
+
+console.log('Hi im pratice about process async')
+setTimeout(() => {
+ console.log('wait here now')
+},4)
+console.log('I go became programer of success in life')
