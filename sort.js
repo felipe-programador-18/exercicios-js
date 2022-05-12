39656a80e12ffb3c73f370e7cebd7fb2db0c0bc2
Felipe <machadofelipe2016@outlook.com>
2022-05-12 14:26:55 -0300

    c171- practice more about that

---
+++ sort.js
@@ -0,0 +1,9 @@
+//practice aobut sort javascript
+
+let string = ['3','3424','34234','434','34324']
+string.sort()
+console.log(string)
+
+let number = ['proammer', 'let go now','programmer more','thought solution']
+number.sort()
+console.log(number)
