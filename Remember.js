2292c7e80bf67db2a19d1b89a33c4ec9feb579f8
Felipe <machadofelipe2016@outlook.com>
2022-05-12 14:48:03 -0300

    c174-practice more

--- Remember.js
+++ Remember.js
@@ -1,7 +1,5 @@
 //4) Dado um vetor de valores, retorne um objeto com quantas vezes cada valor está presente no vetor
-
 const vector = [1223,123,123,123,1,2,3,4,56,7,2,4,5]
-
 const HowmaneTimes = (one,two) =>{
     if(!one[two]){
         one[two] = 0
@@ -12,3 +10,19 @@ const HowmaneTimes = (one,two) =>{
 
 const Thought = vector.reduce(HowmaneTimes,{})
 console.log(Thought)
+
+
+
+
+let names = ['programador','felipe', 'felipe',4324,123,54,556,1,3,54,234,534,123,'felipe']
+
+const Resolve = (mae, filho) => {
+    if(!mae[filho]){
+        mae[filho] = 0
+    }
+    mae[filho] = mae[filho] +1
+    return mae
+}
+
+const Pure = names.reduce(Resolve,{})
+console.log(Pure)
