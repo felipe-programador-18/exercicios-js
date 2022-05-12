c3b89c9240b9afba36536ae5f43703014ab2d43b
Felipe <machadofelipe2016@outlook.com>
2022-05-12 14:26:23 -0300

    c170-try solve issues about code

---
+++ array.calcula.js
@@ -0,0 +1,13 @@
+//Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any //numbers then you should return 0.
+
+
+
+//const tought = (a,b) => a+b
+//const pense = Vector.filter()
+//console.log(Vector.reduce(tought))
+const filtermore =  function(item){
+    return item !== 0
+}
+let Vector = [1,5.2,4,0,,0,0,0,-1].filter(filtermore)
+const Pensa = (a,b) => a+b
+console.log(Vector.reduce(Pensa))
