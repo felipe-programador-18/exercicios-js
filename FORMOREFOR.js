c2321fe1433bab81c019d9d24400a6eba729a507
Felipe <machadofelipe2016@outlook.com>
2022-05-12 14:25:53 -0300

    c169-practice more about for

---
+++ FORMOREFOR.js
@@ -0,0 +1,38 @@
+let str = ''
+for(var i=0; i<10; i++){
+   console.log(`create inside with template string ${i}`)
+   if(i!== 5){
+     console.log('teste more about for')
+   }else{
+       str
+   }
+}
+
+
+for(var i=0; i <46; i++){
+    console.log(`need practice more and more ${i.toFixed(2)}`)
+}
+
+
+let tri = ''
+
+for(let i=0; i<10; i++){
+    tri = tri + i;
+    if(tri > 5){
+        console.log(tri, 'mo')
+    }
+}
+console.log(tri)
+
+
+let contagem = 2
+for(var i =0; i<11; i++){
+    contagem = contagem + i
+    console.log(contagem)
+}
+
+let alfabeto = 'abcd'
+for(var i =0; i<=26; i++){
+    alfabeto = alfabeto+i
+    console.log(alfabeto)
+}
