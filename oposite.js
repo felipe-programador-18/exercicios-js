093d599acc8b8aa427cae8dee17d5e244b00e728
Felipe <machadofelipe2016@outlook.com>
2022-05-14 16:00:13 -0300

    183-adding more

--- oposite.js
+++ oposite.js
@@ -1,4 +1,6 @@
 //very interesting!!!
+
+
 //try understood about that code
 const opposite =(number) =>{
     return number*= -1
@@ -6,3 +8,11 @@ const opposite =(number) =>{
 
 const une = opposite(34)
 console.log(une)
+
+
+const CreateBuld = (one) =>{
+    return one*= -1
+}
+
+const Junta = CreateBuld(-2312312)
+console.log(Junta)
