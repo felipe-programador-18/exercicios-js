c8b9c99ca4d1819061321b88977b2bf6200b33a1
Felipe <machadofelipe2016@outlook.com>
2022-05-14 15:07:23 -0300

    c181-adding more code to  practice more and more

---
+++ moreinestering.js
@@ -0,0 +1,15 @@
+
+
+
+const array = [1,324,234,23,'felipe', undefined, false,"martins", 'programmer18']
+const created = array.filter(Boolean)
+console.log(created)
+
+const Another = array.filter(Number)
+console.log(Another)  
+
+const SolvIssues = ['felipe','programmer', 'devpleno yet','or another still',12]
+const memberaAnother = SolvIssues.filter(Number)
+console.log(memberaAnother)
+
+console.log(memberaAnother)
