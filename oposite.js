a7c5fad7a1b4738d857d7834d3958d54438bced6
Felipe <machadofelipe2016@outlook.com>
2022-05-15 14:18:06 -0300

    c184- practice little bit

--- oposite.js
+++ oposite.js
@@ -16,3 +16,41 @@ const CreateBuld = (one) =>{
 
 const Junta = CreateBuld(-2312312)
 console.log(Junta)
+
+
+const Moroeneod = (two) =>{
+    return two*= -1   
+}
+
+const assey =  Moroeneod(-50)
+console.log(assey)
+
+
+
+const Prenould =  [1,2,31,111,2,3123,123,2,3,2,3,4,5]
+const Like = (solve, resolve) => {
+    if(!solve[resolve]){
+        solve[resolve] = 0
+    }
+    solve[resolve] = solve[resolve]+1
+    return solve
+}
+
+const trouble = Prenould.reduce(Like,{})
+console.log(trouble)
+const kire =  Object.keys(trouble)
+console.log(kire)
+
+
+
+//command concat he adding more element in any thing that create!!
+const Lirea = ['programmer maritns', 123,32,123,13,4512,123]
+const words = Lirea.concat('I go became new programmer react javascript')
+console.log(words)
+
+
+// command pop removed all last element of array
+const Always =  [1,23,123,45,345,12312,3]
+const Mireaea = Always.pop()
+console.log(Mireaea)
+console.log(Always)
---
+++ slice.js
@@ -0,0 +1,5 @@
+const Smore = [12,12,13,123,12,1,2,4]
+Smore.slice(1,2)
+console.log(Smore)
+
+//tomorrow practice about more splice!!! and make some issues!!
