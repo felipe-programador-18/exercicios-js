de27622d2854c0af9847b20f13b20538799b242f
Felipe <machadofelipe2016@outlook.com>
2022-05-18 16:41:31 -0300

    c195-practice more about arrey from and create new file called adding

--- arreyfrom.js
+++ arreyfrom.js
@@ -1,2 +1,10 @@
 console.log(Array.from('foo'))
 console.log(Array.from([1,2,3,4,5,6,], x => x**2))
+
+
+const set = new Set(['foo', 'bar', 'baz', 'foo']);
+ console.log(Array.from(set))
+
+const repeat = new Set(['martins','martins','programmer','thought',1,2,1]) 
+
+console.log(Array.from(repeat))
