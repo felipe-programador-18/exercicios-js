f068c485ed82960b69cfbed633724079f083217a
Felipe <machadofelipe2016@outlook.com>
2022-05-18 17:55:01 -0300

    c199-remake some issues about split

---
+++ split.js
@@ -0,0 +1,11 @@
+//practice little bit about split
+
+let text = "How are you doing today?";
+const myArray = text.split(" ");
+console.log(myArray)
+
+
+//he turn all that string seperate in piece of strings
+let createPore = "I always dreaming pratice more and more always"
+const manage = createPore.split(" ")
+console.log(manage)
