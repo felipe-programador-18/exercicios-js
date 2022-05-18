945cb191968a97bdf6b39070fba46ff4236226df
Felipe <machadofelipe2016@outlook.com>
2022-05-18 16:23:12 -0300

    c191-practice more about code

---
+++ anotherfor.js
@@ -0,0 +1,12 @@
+let stirng = ''
+for(var i = 0; i<7; i++){
+    stirng = stirng +i
+}
+console.log(stirng)
+
+
+let createBuild = ''
+for(var i=0 ; i<5; i++){
+    createBuild = createBuild + i
+}
+console.log(createBuild)
