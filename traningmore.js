96325f3d77ae2c2736a6af94c9cd05ddddb8e672
Felipe <machadofelipe2016@outlook.com>
2022-05-20 15:26:59 -0300

    c201-practice more about code and try understood

---
+++ traningmore.js
@@ -0,0 +1,9 @@
+// practice little bit more and more to evolv !!!
+let text = "How are you doing today?";
+const myArray = text.split(" ");
+console.log(myArray)
+
+const phase = 'thought in solve issues'
+const trire =  phase.split(' ')
+console.log(trire)
+
