efd2849aff786d6c9ee8960c6b480825845b61cb
Felipe <machadofelipe2016@outlook.com>
2022-05-21 19:36:07 -0300

    c216-make more about four operations

--- foursum.js
+++ foursum.js
@@ -1,12 +1,35 @@
-const sum = (a,b) => a*b
+const multiply = (a,b) => a*b
 const operation = (op, a,b) => op(a,b)
 
-const workMe = op =>{
+const split = (a,b) => a/b
+const newoperation = (op, a,b) => op(a,b)
+
+const sum = (a,b) => a+b
+const newoperationsun = (op, a,b) => op(a,b)
+
+const apend = (a,b) => a-b
+const newoperationapend = (op, a,b) => op(a,b)
+
+const basicOp = op =>{
     ops ={
-        "x":op
+        "x":op,
+        "/":op,
+        "+":op,
+        "-":op
     }
     return ops
 }
 
-const multiply = sum(5,4)
-console.log(multiply)
+console.log((basicOp('x',10,2)))
+
+const place = basicOp(12,'x',3)
+console.log(place)
+
+//const double = multiply(5,4)
+//const splitMore = split(10,2)
+//const didSun = sum(10,65) 
+//const apendCode = apend(32,4) 
+//console.log(double)
+//console.log(splitMore)
+//console.log(didSun)
+//console.log(apendCode)
