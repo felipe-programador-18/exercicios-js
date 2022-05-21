26c07781a4087450ff16d60fbd042d0c89a4ed14
Felipe <machadofelipe2016@outlook.com>
2022-05-21 18:38:28 -0300

    c207-adding more about split and reverse commands

---
+++ amass.js
@@ -0,0 +1,22 @@
+//pratice little bit about split
+//and reverse too
+
+const makeSomet = (a) =>{
+    let smarter = a+''
+    smarter = smarter.split("").reverse().map(x => Number(x))
+    return smarter
+}
+
+const notSmarter = makeSomet(50434)
+console.log(notSmarter)
+
+
+const anotherGure = (b) =>{
+    let see = b+''
+    see = see.split("").reverse().map(more => Number(more))
+    return see
+}
+
+const Pire = anotherGure(54250)
+console.log(Pire)
+
