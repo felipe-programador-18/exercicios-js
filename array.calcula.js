8f470d5b81182b6fa7b40d147de4e2c8a17cd483
Felipe <machadofelipe2016@outlook.com>
2022-05-21 16:42:28 -0300

    c206-adding more code to practice

--- array.calcula.js
+++ array.calcula.js
@@ -12,3 +12,26 @@ let Vector = [1,5.2,4,0,,0,0,0,-1].filter(filtermore)
 console.log(Vector)
 const Pensa = (a,b) => a+b
 console.log(Vector.reduce(Pensa))
+
+
+
+
+
+
+
+
+
+
+
+
+
+const removedThem = function(one){
+    return one !== 0 && one >0
+}
+//this code thought in solve and retunr all number that are biggest of 0
+const newVector = [21,32,-1,-3,-4,0,1,2,3,4].filter(removedThem)
+console.log(newVector)
+
+// this case i am making all function to later passed it reduce
+const Testa = (a,b) => a+b
+console.log(newVector.reduce( Testa))
