f679a693ade4d9d5b0f9e52ca3a819363582338e
Felipe <machadofelipe2016@outlook.com>
2022-05-25 15:52:34 -0300

    c227-solve without success

--- petalis.js
+++ petalis.js
@@ -1,19 +1,14 @@
 const howMuchIloveYou = (petalis) => {
-    let saysome = "" + petalis
-    saysome = saysome.split('').map(x => String (x))
-    if(saysome.length >0){
-        console.log('thought slowed')
-    }else{
+    let saysome = ['teste'] +petalis
+    saysome = saysome.split('').map(x => Number (x))
+    if(saysome<=0){
+        console.log('te', saysome)
+    }else if (saysome >3) {
         console.log('not bad')
     }
     return saysome 
 }
-
-const test = howMuchIloveYou()
+const test = howMuchIloveYou(3)
 console.log(test)
 
 
-const saySomething = ['love you','not much','litte bit'].filter(howMuchIloveYou)
-const laineSlowed = saySomething.push(saySomething.length)
-console.log(saySomething)
-console.log('testing solve issues',laineSlowed)
