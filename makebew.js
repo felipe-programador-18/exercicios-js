c3ee493501c8cea447e07b282ffb5fdce4c38634
Felipe <machadofelipe2016@outlook.com>
2022-05-25 16:33:25 -0300

    c231-practice more and more always

---
+++ makebew.js
@@ -0,0 +1,11 @@
+// need practice more  and more everyday!!!
+const funnChallenge = (a,b) => {
+    arr = [];
+    for(i=a; i<=b; i++){
+        arr.push(i)
+    }
+    return arr
+}
+
+const bemoreSmarter = funnChallenge(2,6)
+console.log(bemoreSmarter)
