37f48e31d8465780f8206f5cac8ba8bdd32ba960
Felipe <machadofelipe2016@outlook.com>
2023-03-07 12:05:27 -0300

    c233-to practice a bit

---
+++ FilterFiltermore.js
@@ -0,0 +1,23 @@
+
+
+
+const LeastofHunders = (item) => {
+    return  item <= 100
+}
+
+const allAges = [12,32,4323,23,123,423,4,65,45,2,4,54].filter(LeastofHunders)
+
+console.log(allAges)
+
+const testing = ['felipe',"programmer","felipe", 'programmer more always','more']
+console.log(testing.indexOf('felipe',2))
+
+
+const Swallow = ['morder ', 'mastigar ', 'engolir ', 'what do you prefer?']
+const togetherUnion = (a,b) => a +b
+
+console.log(Swallow.reduce(togetherUnion))
+
+const TestingABit = ['morder ', 'mastigar ', 'engolir ', 'what do you prefer?']
+
+console.log(TestingABit)
