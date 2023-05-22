50fdf60f84897f6fb6e807a1a17b30d496c58b35
felipe-programador-18 <machadofelipe2016@outlook.com>
2023-05-22 14:04:10 -0300

    cto practice a bit

--- codecode.js
+++ codecode.js
@@ -6,3 +6,8 @@ console.log(Object.keys(count))
 
 const Vector =[name='felipe', subname='programmer18']
 console.log(Object.keys(Vector))
+
+
+const getall = (one, tow) => one + tow
+const receiveReducer = count.reduce(getall)
+console.log(`testing my coding here ${receiveReducer} `)
