ea2b211748fc826348932e8271f20debeeb33445
felipe-programador-18 <machadofelipe2016@outlook.com>
2023-05-22 15:57:53 -0300

    to practice a bit more about this

---
+++ anotherfind.js
@@ -0,0 +1,4 @@
+
+const arrayes = [23, 32, 4423, 23, 43]
+const putHereNow = arrayes.find(pega => pega > 23)
+console.log("just doing testing in my application to practice coding a  bit", putHereNow)
