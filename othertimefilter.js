22ff5950f63aaee66a4defc86aa20ac19d85ee17
felipe-programador-18 <machadofelipe2016@outlook.com>
2023-07-28 21:08:03 -0300

    practice a bit javascript to

--- othertimefilter.js
+++ othertimefilter.js
@@ -4,3 +4,23 @@ const filterName = (felipe) => {
 }
 const Names = ['felipe', 'programer'].filter(filterName)
 console.log(Names)
+
+
+
+const CallNames = Names.map((newNames) => {
+  return newNames + " give it all always in accept the new challange that have in my life now" 
+})
+
+console.log(CallNames)
+
+
+const calc =12
+
+if (calc >=10) {
+  console.log ("is biggest than 10" )
+} else if (calc <10) {
+  console.log("is smaller than 10")
+} else {
+  console.log('test')
+}
+console.log(calc)
