a832b50dbc2f01337308d068feeb29884dae7534
felipe-programador-18 <machadofelipe2016@outlook.com>
2023-07-29 12:59:51 -0300

    put my hands-on code

--- saturday.code.js
+++ saturday.code.js
@@ -7,9 +7,10 @@ const PracticeMore = (e, f) => {
 let numberToSum = [323, 123, 45, 4, 23, 2313, 232, 423].map((item) => {
   return item *2
 })
+console.log(numberToSum)
 
 const makeFilter = (item) => {
-  return item <200
+  return item <400
 }
 const ManageAll = numberToSum.filter(makeFilter)
 
