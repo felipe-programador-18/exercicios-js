4592264142db0e37f056e5a2f0640e4d6a128dd5
felipe <machadofelipe2016@outlook.com>
2024-01-04 14:32:31 -0300

    hands on code

--- findIndex.js
+++ findIndex.js
@@ -1,5 +1,5 @@
 
-
+// this funcionality serve to show for my first index after my conditions
 let MoreOneThings = [10,20,30,50,60]
 const joking= MoreOneThings.findIndex(item => item >30)
 console.log(joking)
