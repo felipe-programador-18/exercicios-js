6ba04a46b421e1b11c27bfdcb5fd7fac412822cb
felipe <machadofelipe2016@outlook.com>
2024-01-04 14:32:44 -0300

    to add a bit code about replace

---
+++ replace.js
@@ -0,0 +1,11 @@
+let phrase ='the best way to get their, is just to go straight and turn left'
+//const RemoveSom = phrase.replace("way", 'is')
+console.log(phrase.replace("way", 'SAVED BY HELL'))
+
+
+
+// creare a new function to every moment that I type Felipe is return yep or not
+
+let Name='this guy Felipe will win in the life'
+
+console.log(Name.replace("Felipe", "YES"))
