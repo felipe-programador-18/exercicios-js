1a6cef8675f820a65566aa02bb028fbd67768bd9
felipe <machadofelipe2016@outlook.com>
2024-01-05 11:22:31 -0300

    learn and to sum up more interesting thigns about regex

--- regex.bit.js
+++ regex.bit.js
@@ -1,1 +1,37 @@
 // to practice a  bit about regex
+
+
+const b = /ab+c/
+const manage = new RegExp("abc")
+
+console.log(manage)
+
+
+
+
+var regex = /hello/i;
+var str = 'Hello World';
+
+console.log(regex.test(str));
+
+
+
+
+// to testing if the letter G == global
+
+
+let phrase = /e/g
+const manageAll = 'the best way to go their É'
+
+const ThoughtBro = new RegExp(manageAll)
+
+//console.log(phrase(manageAll))
+
+
+
+// TO PAY ATTENTION ABOUT THIS SMALL STRUCTURE OF CODE!
+// BASICAMENTE O REGEX NESSE CASO QUANDO EU PRECISE BUSCAR VIA SEARCH DIGITANDO ELE NÃO DISTINGUE POR 
+// LETRAS MAISCULAS OU MINUSCULAS
+
+const photos = await PhotoUser.find({title: new RegExp(q,'i')}).exec()
+  console.log('my photos here', photos)
