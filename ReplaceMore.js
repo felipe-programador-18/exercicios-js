29803913189daeadfc1473b249056d1f777c4bc9
felipe <machadofelipe2016@outlook.com>
2024-01-05 09:33:13 -0300

    to practice a bit about replace

---
+++ ReplaceMore.js
@@ -0,0 +1,7 @@
+
+// remind the method replace dont work inside array
+
+let MoreaBit = 'just to putting my hands on code'
+
+const manage =  MoreaBit.replace("putting","more Effor every single day")
+console.log(manage)
