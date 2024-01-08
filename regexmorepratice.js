3da4984ec4e3cc7436669887f2e3d907fbd79d7e
Felipe <machadofelipe2016@outlook.com>
2024-01-07 21:09:46 -0300

    practice a bit more replace and regex

---
+++ regexmorepratice.js
@@ -0,0 +1,15 @@
+
+
+
+let NewNames = 'felipe is probably will to become stand out in life, if keep it up styding and practice everyday'
+
+
+const manage = NewNames.replace("probably",' I NEED JUST MORE TIME AND YEARS TO GET START WORK INTERNATIONALY')
+console.log(manage)
+
+
+let regex  = /testing/i ; 
+let phrase = 'testing here now, TESTING MORE EACH DAY MORE tesTINg'
+
+const allControll = phrase.match(regex)
+console.log(allControll)
