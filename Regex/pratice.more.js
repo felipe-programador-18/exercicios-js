e1b7d7fdf73162ae7fc5121a98b7a5f9fcbe6310
felipe <machadofelipe2016@outlook.com>
2024-01-09 11:23:52 -0300

    to learn about D and d

---
+++ Regex/pratice.more.js
@@ -0,0 +1,22 @@
+
+
+
+const THOUGHT = (phrase) => {  
+let testing =  phrase.replace(/\D/g, '') ;
+
+console.log(testing)
+}
+
+
+THOUGHT("felipe martins 234234324")
+
+
+
+//  this functtion is to return just letters
+function MoveFaster (NoneNumber){
+
+  let Receive = NoneNumber.replace(/\/d/g, '')
+  console.log(Receive)
+}
+
+
