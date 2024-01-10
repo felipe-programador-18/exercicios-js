09234413a14981074a302102b6c0bcff0a2be758
felipe <machadofelipe2016@outlook.com>
2024-01-10 09:35:51 -0300

    to start coding  a bit

---
+++ call.js
@@ -0,0 +1,15 @@
+
+// just to creating a simple function to call my names
+
+function Call (names){
+    return names
+}
+
+
+const RealNames = {
+    nameone:"felipe",
+    nametwo:"martins",
+    namethree:"programmer"
+}
+
+console.log(Call(RealNames.namethree))
