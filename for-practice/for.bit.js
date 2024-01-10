c3ce3c28d812df1f9acded4cf118e5bd9bcadc5c
Felipe <machadofelipe2016@outlook.com>
2024-01-09 22:18:05 -0300

    putting my hands on for structure a bit

---
+++ for-practice/for.bit.js
@@ -0,0 +1,15 @@
+
+
+// putting my hands on code 
+
+let names = ['felipe',' martins', 'programmer','more ']
+
+
+
+for(let i=0; i < names.length; i ++){
+    console.log(names[i])
+}
+
+let NumbersAge = [12,23,45,23,4545,23,434].map((other) => other * 2)
+ 
+console.log(NumbersAge)
