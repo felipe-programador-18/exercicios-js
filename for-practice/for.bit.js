93138579506b2b373ecb666e9603114b9394bb5f
Felipe <machadofelipe2016@outlook.com>
2024-01-09 22:44:43 -0300

    break a bit small strucure of code

--- for-practice/for.bit.js
+++ for-practice/for.bit.js
@@ -13,3 +13,14 @@ for(let i=0; i < names.length; i ++){
 let NumbersAge = [12,23,45,23,4545,23,434].map((other) => other * 2)
  
 console.log(NumbersAge)
+
+
+
+
+let SumByAyy = [1,3,7,9,11]
+
+for(var i=0; i < SumByAyy.length; i++){
+    SumByAyy[i] = SumByAyy[i] * 3
+
+    console.log(SumByAyy)
+}
