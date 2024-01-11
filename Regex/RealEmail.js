4fbe9f54ebff7a9d27063068d705de9f27d6cd99
felipe <machadofelipe2016@outlook.com>
2024-01-11 11:28:27 -0300

    learning about regex and email

---
+++ Regex/RealEmail.js
@@ -0,0 +1,9 @@
+
+
+
+let PrhaseAndEmail ="lemme typing my email here now: machadofelipe@outlook.com "
+
+// to practice a lot about this small thing!
+let regex= /([a-z0-9]{2,})@([a-z0-9]{2,})(\.[a-z]{2,})(\.[a-z]{2,})?/g ; 
+let manageAll=PrhaseAndEmail.match(regex)
+console.log(manageAll)
